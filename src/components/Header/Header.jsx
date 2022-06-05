6bbb342d0de6c3f76f9ede69a29a7c1785bedd09
furkaner <furkan08_@hotmail.com>
2022-06-05 23:51:57 +0300

    header added reset fonksiyon added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { useSelector } from 'react-redux'
-import { pointSelector } from '../../features/gameSlice'
-const Header = () => {
-    const score = useSelector(pointSelector)
+import React from "react";
+//styles
+import styles from "./Header.module.css";
+//redux
+import { useSelector, useDispatch } from "react-redux";
+import { pointSelector, initialCards, resetCard } from "../../features/gameSlice";
+const Header = ({reset}) => {
+  const dispatch = useDispatch();
+  const score = useSelector(pointSelector);
+
+  const handleClick=()=>{
+      dispatch(resetCard())
+     // dispatch(initialCards())
+     setTimeout(() => {
+      reset();
+    }, 1000);
+      console.log('çalişti');
+  }
   return (
     <main className={styles.container}>
-        <article className={styles.PointContainer}>
-            <p>Point: {score}</p>
-            <div className={styles.reStartButton}>Restart Game</div>
-        </article>
+      <article className={styles.PointContainer}>
+        <p>Point: {score}</p>
+        <div className={styles.reStartButton} onClick={handleClick}>
+          Restart Game
+        </div>
+      </article>
     </main>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
