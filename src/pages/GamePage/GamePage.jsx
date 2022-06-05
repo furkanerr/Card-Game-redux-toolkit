a91ae63ccaf175b7d0eb87abd0a1adf991cd77ea
furkaner <furkan08_@hotmail.com>
2022-06-05 19:55:29 +0300

    game logic added

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,22 +1,78 @@
-import {useEffect} from 'react'
+import { useEffect, useState } from "react";
 
 //redux
-import {initialCards} from '../../features/gameSlice'
-import { useSelector, useDispatch } from 'react-redux'
+import {
+  initialCards,
+  cardsSelector,
+  increaseScore,
+  decreaseScore,
+  matched,
+ 
+} from "../../features/gameSlice";
+import { useSelector, useDispatch } from "react-redux";
 
+//styles
+import styles from "./GamePage.module.css";
+//Components
+import Card from "../../components/Card/Card";
+import Header from "../../components/Header/Header";
 
 const GamePage = () => {
-    const dispatch = useDispatch();
-  const game = useSelector((state)=>state.game)
+  const [item1, setItem1] = useState(null);
+  const [item2, setItem2] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const dispatch = useDispatch();
+  const Cards = useSelector(cardsSelector);
   useEffect(() => {
-    dispatch(initialCards())
+    dispatch(initialCards());
+  }, [dispatch]);
 
+  useEffect(() => {
+    if (item1 && item2) {
+      setDisabled(true);
+      if (item1.src === item2.src) {
+        dispatch(increaseScore());
+        dispatch(matched(item1));
+        reset();
+      } else {
+        dispatch(decreaseScore());
+        setTimeout(() => {
+          reset();
+        }, 1000);
+      }
+    }
+  }, [item1, item2, dispatch]);
 
-  }, [])
-  
+  const reset = () => {
+    setItem1(null);
+    setItem2(null);
+
+    setDisabled(false);
+  };
+  const handleClick = (card) => {
+    item1 && item1 !== card ? setItem2(card) : setItem1(card);
+  };
   return (
-    <div>GamePage</div>
-  )
-}
+    <>
+      <div className={styles.Container}>
+        <Header />
+        <div className={styles.GameContainer}>
+          {Cards.map((card, index) => {
+            return (
+              <Card
+                key={index}
+                card={card}
+                handleSelectedCard={handleClick}
+                selected={card === item1 || card === item2}
+                disabled={disabled}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default GamePage
+export default GamePage;
