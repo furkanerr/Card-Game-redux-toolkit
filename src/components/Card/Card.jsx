d87e1b4b81654a55940ab4731760d35784d5df58
furkaner <furkan08_@hotmail.com>
2022-06-05 19:54:32 +0300

    flipped and disabled added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
 
 //Styles
-import styles from './Card.module.css';
+import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ card, handleSelectedCard, disabled, selected }) => {
+  //console.log(selected)
+  const handleClick = () => {
+    if (disabled === false) {
+      handleSelectedCard(card);
+    }
+  };
   return (
-    <div>
-        Card
-
+    <div className="card">
+      <div
+        className={
+          card.matched === true || selected ? styles.flipped : styles.Container
+        }
+        onClick={() => handleClick()}
+      >
+        <img className={styles.front} src={card.src} alt="resim" />
+        <div className={styles.back}>?</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
