7023a87873200d956a2320d154874ec0d83ca430
furkaner <furkan08_@hotmail.com>
2022-06-06 21:56:00 +0300

    local storage added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 import styles from './HomePage.module.css';
 const HomePage = () => {
+    const[point,setPoint] = useState(0);
+  useEffect(() => {
+      setPoint(localStorage.getItem('point'))
+  }, [])
+
+
+
+
   return (
       
     <main className={styles.Container}>
         <section className={styles.PointScreen}>
-        <p>Total Point: 100</p>
+        <p>Total Point: {point ? point : 0}</p>
         <Link to={'/game'}className={styles.StartButton}>
         <div >Start</div>
         </Link>
